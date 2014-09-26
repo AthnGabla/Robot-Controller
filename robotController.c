3da4950708dd20d12c362df323daafcf76392b94
CryHavoc21 <cryhavoc24@gmail.com>
2014-09-25 19:32:22 -0500

    test

--- robotController.c
+++ robotController.c
@@ -1,8 +1,18 @@
+#pragma config(Motor,motor1,leftMotor,tmotorVexIQ, openLoop, encoder)
+#pragma config(Motor,motor6,rightMotor,tmotorVexIQ, openLoop, reversed, encoder)
 
+//int a = 0;
+//int speedMultiplier = 1;
 
 task main(){
-
-
-
-
+//	a = getBumperValue(port2
+/*
+	if(a == 0){
+		setMotorSpeed(leftMotor, 127);
+		setMotorSpeed(rightMotor, 127);
+	}
+*/
+	setMotorSpeed(leftMotor,  (getJoystickValue(ChA) + getJoystickValue(ChB))/2);  	// (y - x)/2
+	setMotorSpeed(rightMotor, (getJoystickValue(ChA) - getJoystickValue(ChB))/2);  	// (y + x)/2
+startTask(main);
 }
