c400062618003c9d270bc8cee89cfa45796d53b8
CryHavoc21 <cryhavoc24@gmail.com>
2014-09-26 19:51:50 -0500

    Commit of Initial Program
    Added program with #pragma

--- robotController.c
+++ robotController.c
@@ -1,18 +1,30 @@
-#pragma config(Motor,motor1,leftMotor,tmotorVexIQ, openLoop, encoder)
-#pragma config(Motor,motor6,rightMotor,tmotorVexIQ, openLoop, reversed, encoder)
+#pragma config(Motor,port2,rightLarge,tmotorNormal,openLoop, reversed)
+#pragma config(Motor,port3,rightSmall,tmotorNormal,openLoop, reversed)
+#pragma config(Motor,port4,servoMotorA,tmotorNormal,openLoop)
+#pragma config(Motor,port5,servoMotorB,tmotorNormal,openLoop)
+#pragma config(Motor,port6,servoMotorC,tmotorNormal,openLoop)
+#pragma config(Motor,port7,servoMotorD,tmotorNormal,openLoop)
+#pragma config(Motor,port8,leftSmall,tmotorNormal,openLoop)
+#pragma config(Motor,port9,leftLarge,tmotorNormal,openLoop)
 
-//int a = 0;
-//int speedMultiplier = 1;
+//protip: set a motor speed or a servo position by setting the object of the motor array at that position
+//to either a value from -127 to 127
+//get a controller's value with the function "vexRT(channel);"
+
+int a = 0;
+int speedMultiplier = 1;
 
 task main(){
-//	a = getBumperValue(port2
-/*
-	if(a == 0){
-		setMotorSpeed(leftMotor, 127);
-		setMotorSpeed(rightMotor, 127);
+
+	if(vexRT(Ch1) > 10){
+		motor[servoMotorA] = 127;
 	}
-*/
-	setMotorSpeed(leftMotor,  (getJoystickValue(ChA) + getJoystickValue(ChB))/2);  	// (y - x)/2
-	setMotorSpeed(rightMotor, (getJoystickValue(ChA) - getJoystickValue(ChB))/2);  	// (y + x)/2
-startTask(main);
+	else if(vexRT(Ch1) < -10){
+		motor[servoMotorA] = -127;
+	}
+
+//	motor[leftLarge] = 127;
+//	motor[rightLarge] = 127;
+
+	startTask(main);
 }
